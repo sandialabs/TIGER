0ca2258de251ba651e6cb73d704340abee19dfc8
infurnoheat <stevenleeyu@gmail.com>
2022-08-25 11:33:18 -0700

    Typing Bug Fizes

--- bin/phage.pl
+++ bin/phage.pl
@@ -11,7 +11,7 @@ my $dir = File::Spec->rel2abs($0); $dir =~ s/\/([^\/]+)$//;
 my $file = $ARGV[0];
 $file = File::Spec->rel2abs($file);
 die unless $file =~ /([^\/]+)\/([^\/]+)\.[^\.]+$/;
-$name = $1;
+$name = $1; $name =~ s/\.x//;
 $nick = $2; 
 
 system("perl $dir/tater.pl $file") unless -f "$nick.gff";
@@ -36,10 +36,9 @@ for (`cat $nick.gff`){
  while ($f[8] =~ s/^([^=;]+)=([^;]+);//g) {$l{$1} = $2};
  if ($l{pfam1} ~~ \@phage_structure) {push @struct, $l{ID}, push @sname, $l{pfam1}}
  elsif ($l{pfam1} ~~ @phage_funct) {push @funct, $l{ID}, push @fname, $l{pfam1}}
- $Stot = scalar @sname; 
- $Ftot = scalar @fname;
 }
-
+$Stot = scalar @sname;
+$Ftot = scalar @fname;
 
 my $len = 0; for (`cat $file`) {next if /^>/; chomp; $len += length $_;}
 if (grep {$_ eq 'Zot'} @fname and $len < 13000) {$phagecall = 4} 
@@ -50,9 +49,5 @@ elsif ($Ftot >= 1) {$phagecall = 3}
 
 open (OUT, "> phage.txt");
 print OUT "#nick\tstructTot\tnonStructTot\tphagecall\tstructural\tnonStruct\n";
-$name =~ s/.*\///;
 print OUT "$name\t$Stot\t$Ftot\t$phagecall\t", join(',', @sname), "\t", join(',', @fname), "\n";
 close (OUT);
-
-
-
