4be3374741928af97091244011ecbc3fa821e3e1
infurnoheat <stevenleeyu@gmail.com>
2024-01-15 22:52:00 -0800

    Revert "New Resolve and Update db"
    This reverts commit 95a6c19437dec2abe279a849c2a3473f60fe5f71.

--- bin/splitFa.pl
+++
@@ -1,8 +0,0 @@
-for (`cat isles.fa`) {
- if (/>([^\s\|\+\']+)/) {#close OUT; 
-  my $nick = $1;
-  my $head = substr($nick, 0, 18);
- system ("mkdir Isles/$nick"); 
- open OUT, ">Isles/$nick/genome.fa"}
- print OUT $_;
-}
